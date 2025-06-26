0eee4dc37046f1c4aae75fbfd9d89325655bcc85
filipguz <filip@gustavsenfriluft.no>
2025-06-26 11:01:26 +0200

    fiks av sletting leir/liste

--- src/main/java/com/example/LeirskolePortalen/model/Leir.java
+++ src/main/java/com/example/LeirskolePortalen/model/Leir.java
@@ -5,10 +5,9 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-
-
 @Entity
 public class Leir {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -21,12 +20,7 @@ public class Leir {
     private Integer antallJenter;
     private Integer antallGutter;
     private Integer antallElever;
-
     private Integer uke;
-
-    //private boolean sendtOpplysninger;
-    //private boolean mottattBekreftelse;
-    //private boolean mottattBestilling;
 
     @Column(length = 1000)
     private String merknader;
@@ -34,35 +28,20 @@ public class Leir {
     @ManyToOne
     private Bruker bruker;
 
-    @OneToMany(mappedBy = "leir")
-    private List<Hytte> hytter;
+    @OneToMany(mappedBy = "leir", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Hytte> hytter = new ArrayList<>();
 
-    @OneToMany(mappedBy = "leir")
-    private List<Deltaker> deltakere;
+    @OneToMany(mappedBy = "leir", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Deltaker> deltakere = new ArrayList<>();
 
-    @ManyToMany // eller @OneToMany, avhengig av relasjonen
+    @ManyToMany
     private List<Skole> skoler = new ArrayList<>();
 
     @ManyToOne
-    @JoinColumn(name = "skole_id") // Valgfritt: navnet på kolonnen i databasen
+    @JoinColumn(name = "skole_id")
     private Skole skole;
 
-
-
-
     // ---------- Getters og Setters ----------
-
-    public List<Skole> getSkoler() {
-        return skoler;
-    }
-
-    // Setter for skoler
-    public void setSkoler(List<Skole> skoler) {
-        this.skoler = skoler;
-    }
-
-    public Skole getSkole() { return skole; }
-    public void setSkole(Skole skole) { this.skole = skole; }
 
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
@@ -91,15 +70,6 @@ public class Leir {
     public Integer getUke() { return uke; }
     public void setUke(Integer uke) { this.uke = uke; }
 
-    //public boolean isSendtOpplysninger() { return sendtOpplysninger; }
-   // public void setSendtOpplysninger(boolean sendtOpplysninger) { this.sendtOpplysninger = sendtOpplysninger; }
-
-    //public boolean isMottattBekreftelse() { return mottattBekreftelse; }
-    //public void setMottattBekreftelse(boolean mottattBekreftelse) { this.mottattBekreftelse = mottattBekreftelse; }
-
-    //public boolean isMottattBestilling() { return mottattBestilling; }
-    //public void setMottattBestilling(boolean mottattBestilling) { this.mottattBestilling = mottattBestilling; }
-
     public String getMerknader() { return merknader; }
     public void setMerknader(String merknader) { this.merknader = merknader; }
 
@@ -112,81 +82,10 @@ public class Leir {
     public List<Deltaker> getDeltakere() { return deltakere; }
     public void setDeltakere(List<Deltaker> deltakere) { this.deltakere = deltakere; }
 
-    //public List<SkolePlan> getSkoleplan() { return skoleplan; }
-   // public void setSkoleplan(List<SkolePlan> skoleplan) { this.skoleplan = skoleplan; }
+    public List<Skole> getSkoler() { return skoler; }
+    public void setSkoler(List<Skole> skoler) { this.skoler = skoler; }
+
+    public Skole getSkole() { return skole; }
+    public void setSkole(Skole skole) { this.skole = skole; }
 }
 
-
-
-
-
-
-
-
-/*
-@Entity
-public class Leir {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private String navn;
-    private LocalDate startDato;
-    private LocalDate sluttDato;
-    private String ansvarligLaerer;
-
-    @OneToMany(mappedBy = "leir")
-    private List<Hytte> hytter;
-
-    @OneToMany(mappedBy = "leir")
-    private List<Deltaker> deltakere;
-
-    @OneToMany(mappedBy = "leir")
-    private List<SkolePlan> skoleplan;
-
-    /*@Column(name = "logo_url")
-    private String logoUrl;
-
-    public String getLogoUrl() {
-        return logoUrl;
-    }
-
-    public void setLogoUrl(String logoUrl) {
-        this.logoUrl = logoUrl;
-    }
-
-
-
-    public void setSkoleplan(List<SkolePlan> skoleplan) {
-        this.skoleplan = skoleplan;
-    }
-    public List<SkolePlan> getSkoleplan() {
-        return skoleplan;
-    }
-    @ManyToOne
-    private Bruker bruker;
-
-    // Getters and setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public String getNavn() { return navn; }
-    public void setNavn(String navn) { this.navn = navn; }
-
-    public LocalDate getStartDato() { return startDato; }
-    public void setStartDato(LocalDate startDato) { this.startDato = startDato; }
-
-    public LocalDate getSluttDato() { return sluttDato; }
-    public void setSluttDato(LocalDate sluttDato) { this.sluttDato = sluttDato; }
-
-    public String getAnsvarligLaerer() { return ansvarligLaerer; }
-    public void setAnsvarligLaerer(String ansvarligLaerer) { this.ansvarligLaerer = ansvarligLaerer; }
-
-    public List<Hytte> getHytter() { return hytter; }
-    public void setHytter(List<Hytte> hytter) { this.hytter = hytter; }
-
-    public List<Deltaker> getDeltakere() { return deltakere; }
-    public void setDeltakere(List<Deltaker> deltakere) { this.deltakere = deltakere; }
-}
-
- */
